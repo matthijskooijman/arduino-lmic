1b796c178dc55001f224de758e6d5828a1d0bc49
Matthijs Kooijman <matthijs@stdin.nl>
2016-02-03 22:09:21 +0100

    Add example sketch for LoRaWAN
    This sketch is aimed at The Things Network, but it should work for other
    networks as well. It employs personalization, over-the-air activation
    has not been tested yet.

--- examples/raw/raw.ino
+++ examples/raw/raw.ino
@@ -37,15 +37,13 @@ const lmic_pinmap lmic_pins = {
     .dio = {2, 3, 4},
 };
 
-//LMIC application callbacks not used in his example
-void os_getArtEui (u1_t* buf) {
-}
 
-void os_getDevEui (u1_t* buf) {
-}
-
-void os_getDevKey (u1_t* buf) {
-}
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
 
 void onEvent (ev_t ev) {
 }
