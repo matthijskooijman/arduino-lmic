cbd1d239cb13979c78dfa5b7df0900cf6ead2d5b
Matthijs Kooijman <matthijs@stdin.nl>
2016-02-20 11:49:12 +0100

    Change the default radio to SX1276
    This is the recommended radio for new designs, so it seems more likely
    people will be starting out with that one.

--- src/lmic/config.h
+++ src/lmic/config.h
@@ -7,8 +7,12 @@
 
 #define CFG_eu868 1
 //#define CFG_us915 1
-#define CFG_sx1272_radio 1
-//#define CFG_sx1276_radio 1
+// This is the SX1272/SX1273 radio, which is also used on the HopeRF
+// RFM92 boards.
+//#define CFG_sx1272_radio 1
+// This is the SX1276/SX1277/SX1278/SX1279 radio, which is also used on
+// the HopeRF RFM95 boards.
+#define CFG_sx1276_radio 1
 
 // 16 μs per tick
 // LMIC requires ticks to be 15.5μs - 100 μs long
