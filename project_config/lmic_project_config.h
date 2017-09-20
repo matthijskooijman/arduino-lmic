c46835cc42db06a1fcf12a11726614b66b6763a4
Terry Moore <tmm@mcci.com>
2017-09-20 01:44:52 -0400

    Change default region back to US

--- project_config/lmic_project_config.h
+++ project_config/lmic_project_config.h
@@ -1,8 +1,8 @@
 // project-specific definitions for otaa sensor
 //#define CFG_eu868 1
-//#define CFG_us915 1
+#define CFG_us915 1
 //#define CFG_au921 1
 //#define CFG_as923 1
-#define CFG_in866 1
+//#define CFG_in866 1
 #define CFG_sx1276_radio 1
 //#define LMIC_USE_INTERRUPTS
