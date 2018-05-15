378506d159bfcd4f5cf047b0f79bd1df2f42ccf7
Shoichi Sakane <github-shoichi@tanu.org>
2018-05-16 01:19:34 +0900

    defined AS923JP_LBT_5MS for Japan regulation.  It assumes to use from 920.6 to 923.4 and the longer sensing time.  128us of the sensing time for 922.4 or upper freqs. is not supported yet.

--- src/lmic/lorabase_as923.h
+++ src/lmic/lorabase_as923.h
@@ -74,7 +74,7 @@ enum { DR_PAGE_AS923 = 0x10 * (LMIC_REGION_as923 - 1) };
 
 enum { AS923_LMIC_REGION_EIRP = 1 };         // region uses EIRP
 
-enum { AS923JP_LBT_US = 125 };          // microseconds of LBT time
+enum { AS923JP_LBT_5MS = 5 };          // microseconds of LBT time
 enum { AS923JP_LBT_DB_MAX = -80 };      // maximum channel strength
 
 #endif /* _lorabase_as923_h_ */
