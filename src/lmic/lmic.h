a72fff13785dc09444c7500c9f04a7e1e6d22aaa
Nicolas Graziano <nicolas.graziano@engie.com>
2018-11-26 18:04:48 +0100

    Correct ADR ACK request limit.
    "After ADR_ACK_LIMIT uplinks (ADR_ACK_CNT >=ADR_ACK_LIMIT) without any downlink response, it sets the ADR acknowledgment request bit (ADRACKReq)"
    "The network is required to respond with a downlink frame within the next ADR_ACK_DELAY frames, any received downlink frame following an uplink frame resets the ADR_ACK_CNT counter".
    
    LMIC set ADRACKReq if LMIC.adrAckReq (ie. ADR_ACK_CNT) is > 0 and set link down if > LINK_CHECK_INIT.
    
    So set LINK_CHECK_INIT to -ADR_ACK_LIMIT and LINK_CHECK_DEAD to ADR_ACK_DELAY.

--- src/lmic/lmic.h
+++ src/lmic/lmic.h
@@ -127,9 +127,9 @@ enum { TXCONF_ATTEMPTS    =   8 };   //!< Transmit attempts for confirmed frames
 enum { MAX_MISSED_BCNS    =  20 };   // threshold for triggering rejoin requests
 enum { MAX_RXSYMS         = 100 };   // stop tracking beacon beyond this
 
-enum { LINK_CHECK_CONT    =  12 ,    // continue with this after reported dead link
-       LINK_CHECK_DEAD    =  24 ,    // after this UP frames and no response from NWK assume link is dead
-       LINK_CHECK_INIT    = -12 ,    // UP frame count until we inc datarate
+enum { LINK_CHECK_CONT    =  0  ,    // continue with this after reported dead link
+       LINK_CHECK_DEAD    =  32 ,    // after this UP frames and no response to ack from NWK assume link is dead (ADR_ACK_DELAY)
+       LINK_CHECK_INIT    = -64 ,    // UP frame count until we ark for ack (ADR_ACK_LIMIT)
        LINK_CHECK_OFF     =-128 };   // link check disabled
 
 enum { TIME_RESYNC        = 6*128 }; // secs
