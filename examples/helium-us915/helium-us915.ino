592f5fba29b2ab6dd032cb1521518f0c5d87f238
Louis Thiery <louis@helium.com>
2020-02-01 14:17:14 -0800

    add Catena 4610 and Disco L072CZ to Helium example

--- examples/helium-us915/helium-us915.ino
+++ examples/helium-us915/helium-us915.ino
@@ -118,6 +118,14 @@ const lmic_pinmap lmic_pins = {
         .rssi_cal = 10,
         .spi_freq = 8000000     // 8MHz
 };
+#elif defined(MCCI_CATENA_4610) 
+#include "arduino_lmic_hal_boards.h"
+const lmic_pinmap lmic_pins = *Arduino_LMIC::GetPinmap_Catena4610();
+#elif defined(ARDUINO_DISCO_L072CZ_LRWAN1)
+#include "arduino_lmic_hal_boards.h"
+// Pin mapping Discovery 
+const lmic_pinmap lmic_pins = *Arduino_LMIC::GetPinmap_Disco_L072cz_Lrwan1();
+#else
 # error "Unknown target"
 #endif
 
